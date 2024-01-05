cf8f2c6fdd93b49f967e4b537f72bec0e1d4b1e4
SirBernhart <behornerl@gmail.com>
2024-01-04 21:36:15 -0300

    ProductConfig.cs - Remove CompareTo method

--- Mobile potion 1/Assets/Scripts/Recipes/ProductConfig.cs
+++ Mobile potion 1/Assets/Scripts/Recipes/ProductConfig.cs
@@ -1,16 +1,9 @@
 using System;
 using UnityEngine;
 
-public abstract class ProductConfig : ScriptableObject, IComparable
+public abstract class ProductConfig : ScriptableObject
 {
     public string Name;
     public string Description;
     public Sprite Sprite;
-
-    public int CompareTo(object obj)
-    {
-        var otherConfig = (ProductConfig) obj;
-
-        return Name.CompareTo(otherConfig.Name);
-    }
 }
