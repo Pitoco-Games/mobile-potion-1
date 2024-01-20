d949670fd14b90fa6b8a737188b80b4558e354ea
SirBernhart <behornerl@gmail.com>
2024-01-20 15:44:00 -0300

    IProductContainer.cs - Add new Action<bool> parameter to TryTakeProduct

--- Mobile potion 1/Assets/Scripts/Controls/IProductContainer.cs
+++ Mobile potion 1/Assets/Scripts/Controls/IProductContainer.cs
@@ -1,4 +1,6 @@
+using System;
+
 public interface IProductContainer
 {
-    bool TryTakeProduct(out ProductWithState productData);
+    bool TryTakeProduct(out ProductWithState productData, out Action<bool> onProductReleased);
 }
